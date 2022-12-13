69f5b190609b6cd3d20dfcb101a41db36a5667ef
ikayden <ikyden23@gmail.com>
2022-12-12 18:56:36 -0800

    solved code challenge 4

--- fundamentals_part2/script.js
+++ fundamentals_part2/script.js
@@ -57,29 +57,48 @@
 
 // Coding challenge #3
 
-const mark = {
-  fullName: ["Mark", "Miller"],
-  mass: 78,
-  height: 1.69,
-  calcBMI: function() {
-    this.BMI = Math.round(this.mass / (this.height ** 2));
-    return this.BMI;
-  }
-};
-const john = {
-  fullName: ["John", "Smith"],
-  mass: 92,
-  height: 1.95,
-  calcBMI: function() {
-    this.BMI = Math.round(this.mass / (this.height ** 2));
-    return this.BMI;
-  }
-};
+// const mark = {
+//   fullName: ["Mark", "Miller"],
+//   mass: 78,
+//   height: 1.69,
+//   calcBMI: function() {
+//     this.BMI = Math.round(this.mass / (this.height ** 2));
+//     return this.BMI;
+//   }
+// };
+// const john = {
+//   fullName: ["John", "Smith"],
+//   mass: 92,
+//   height: 1.95,
+//   calcBMI: function() {
+//     this.BMI = Math.round(this.mass / (this.height ** 2));
+//     return this.BMI;
+//   }
+// };
 
-mark.calcBMI();
-john.calcBMI();
+// mark.calcBMI();
+// john.calcBMI();
 
-console.log(
-  `${mark.fullName[0]}'s BMI (${mark.BMI}) is ${mark.BMI > john.BMI ? "higher than" : "lower than "}John's (${john.BMI})`
-)
+// console.log(
+//   `${mark.fullName[0]}'s BMI (${mark.BMI}) is ${mark.BMI > john.BMI ? "higher than" : "lower than "}John's (${john.BMI})`
+// )
 
+// Coding challenge #4
+
+const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+
+const tips = [];
+const totals = [];
+
+const calcTip = bill =>
+  bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.20;
+
+for (let i = 0; i < bills.length; i++) {
+  let tip = calcTip(bills[i]);
+  let total = tip + bills[i];
+  tips.push(tip);
+  totals.push(total);
+}
+
+console.log(tips);
+console.log(totals);
