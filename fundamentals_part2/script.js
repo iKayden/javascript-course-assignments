7755fa32cf5b3d7226c37dc56d59f62d2dba3733
ikayden <ikyden23@gmail.com>
2022-12-12 23:24:06 -0800

    Opposites Attract finish challenge

--- fundamentals_part2/script.js
+++ fundamentals_part2/script.js
@@ -117,3 +117,36 @@
 // calcAverage(tips);
 // calcAverage(bills);
 // calcAverage(totals);
+
+/* Extra code challenge
+Timmy & Sarah think they are in love, but around where they live, they will only know once they pick a flower each. If one of the flowers has an even number of petals and the other has an odd number of petals it means they are in love.
+
+Write a function that will take the number of petals of each flower and return true if they are in love and false if they aren't.
+*/
+
+function lovefunc(flower1, flower2) {
+  // moment of truth
+  if (flower1 % 2 === 0 && flower2 % 2 === 0) {
+    return false;
+  } else if (flower1 % 2 === 0 && flower2 % 2 === 1) {
+    return true;
+  } else if (flower2 % 2 === 0 && flower1 % 2 === 1) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+const randNum1 = Math.floor(Math.random() * 100);
+// console.log("num 1 ", randNum1);
+const randNum2 = Math.floor(Math.random() * 100);
+// console.log("num 2 ", randNum2);
+
+const result = lovefunc(1, 4); //true
+const result2 = lovefunc(2, 2);// false
+const result3 = lovefunc(0, 1);// true
+const result4 = lovefunc(0, 0);// false
+const result5 = lovefunc(423, 612); //true
+const result6 = lovefunc(671, 114); //true
+
+console.log(result6);
