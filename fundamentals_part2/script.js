6f3d325a19fbf45b41542172adc298d48e29fc10
ikayden <ikyden23@gmail.com>
2022-12-15 12:56:16 -0800

    add ideal solution function

--- fundamentals_part2/script.js
+++ fundamentals_part2/script.js
@@ -371,15 +371,20 @@ Notes
 All numbers are valid Int32, no need to validate them.
 There will always be at least one number in the input string.
 Output string must be two numbers separated by a single space, and highest number is first. */
+// My Function
+// function highAndLow(numbers) {
+//   const sortArr = numbers.split(" ").sort((a, b) => a - b).reverse();
+//   const output = `${sortArr[0]} ${sortArr[sortArr.length - 1]}`;
+//   return output;
+// }
+// Ideal one
+// function highAndLow(numbers) {
+//   numbers = numbers.split(' ');
+//   return `${Math.max(...numbers)} ${Math.min(...numbers)}`;
+// }
 
-function highAndLow(numbers) {
-  const sortArr = numbers.split(" ").sort((a, b) => a - b).reverse();
-  const output = `${sortArr[0]} ${sortArr[sortArr.length - 1]}`;
-  return output;
-}
-
-// Examples
-highAndLow("1 2 3 4 5");  // return "5 1"
-highAndLow("1 2 -3 4 5"); // return "5 -3"
-highAndLow("1 9 3 4 -5"); // return "9 -5"
-highAndLow("8 3 -5 42 -1 0 0 -9 4 7 4 -4");// "42 -9"
+// // Examples
+// highAndLow("1 2 3 4 5");  // return "5 1"
+// highAndLow("1 2 -3 4 5"); // return "5 -3"
+// highAndLow("1 9 3 4 -5"); // return "9 -5"
+// highAndLow("8 3 -5 42 -1 0 0 -9 4 7 4 -4");// "42 -9"
