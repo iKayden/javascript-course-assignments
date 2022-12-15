5723ab06c65ee8f2058c21c8e48f7eabeddd812f
ikayden <ikyden23@gmail.com>
2022-12-15 12:23:36 -0800

    Descending Order Task Done

--- fundamentals_part2/script.js
+++ fundamentals_part2/script.js
@@ -345,3 +345,21 @@ setAlarm(true, false) -> true */
 // function setAlarm(employed, vacation) {
 //   return employed && !vacation ? true : false;
 // }
+
+/* Your task is to make a function that can take any non-negative integer as an argument and return it with its digits in descending order. Essentially, rearrange the digits to create the highest possible number.
+
+Examples:
+Input: 42145 Output: 54421
+
+Input: 145263 Output: 654321
+
+Input: 123456789 Output: 987654321 */
+
+// function descendingOrder(n) {
+//   if (n <= 0) return 0;
+//   const numArr = String(n).split("").sort().reverse().join("");
+//   return Number(numArr);
+// }
+
+// descendingOrder(42145);
+// descendingOrder(145263);
