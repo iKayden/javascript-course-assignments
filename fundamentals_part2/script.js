599497de3d7520fab73fa8521111fa9729b59ecb
ikayden <ikyden23@gmail.com>
2022-12-16 02:25:14 -0800

    add close modal func, refactor

--- fundamentals_part2/script.js
+++ fundamentals_part2/script.js
@@ -402,3 +402,20 @@ For example, when an array is passed like [19, 5, 42, 2, 77], the output should 
 
 // sumTwoSmallestNumbers([5, 8, 12, 19, 22]); // 13
 // sumTwoSmallestNumbers([15, 28, 4, 2, 43]); // 6
+
+/* Given two integers a and b, which can be positive or negative, find the sum of all the integers between and including them and return it. If the two numbers are equal return a or b.
+
+
+Note: a and b are not ordered! */
+function getSum(a, b) {
+  let min = Math.min(a, b);
+  let max = Math.max(a, b);
+  return (max - min + 1) * (min + max) / 2;
+
+}
+
+
+
+getSum(1, 1);//-- > 1(1 )
+getSum(-1, 0);//-- > -1(-1 + 0 = -1)
+getSum(-1, 2);//-- > 2(-1 + 0 + 1 + 2 = 2);
--- modal_part3/script.js
+++ modal_part3/script.js
@@ -5,8 +5,19 @@ const overlay = document.querySelector(".overlay");
 const closeBtn = document.querySelector(".close-modal");
 const showBtns = document.querySelectorAll(".show-modal");
 
-for (const showBtn of showBtns) {
-  showBtn.addEventListener("click", function() {
-    modal.classList.remove("hidden");
-  });
+function closeModal() {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
 }
+function openModal() {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+
+}
+for (const showBtn of showBtns) {
+  showBtn.addEventListener("click", openModal);
+}
+
+closeBtn.addEventListener("click", closeModal);
+
+overlay.addEventListener("click", closeModal);
