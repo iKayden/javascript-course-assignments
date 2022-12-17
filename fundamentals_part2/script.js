a8cec1617e8434ae553c221e0e24c98845cf1e5c
ikayden <ikyden23@gmail.com>
2022-12-17 03:19:19 -0800

    Complementary DNA code challenge

--- fundamentals_part2/script.js
+++ fundamentals_part2/script.js
@@ -507,3 +507,49 @@ String will never be empty and you do not need to account for different data typ
 
 // const boolToWord = bool => bool ? "Yes" : "No";
 // console.log(boolToWord(true)); // 'Yes';
+
+/* Complementary DNA
+Deoxyribonucleic acid (DNA) is a chemical found in the nucleus of cells and carries the "instructions" for the development and functioning of living organisms.
+
+If you want to know more: http://en.wikipedia.org/wiki/DNA
+
+In DNA strings, symbols "A" and "T" are complements of each other, as "C" and "G". Your function receives one side of the DNA (string, except for Haskell); you need to return the other complementary side. DNA strand is never empty or there is no DNA at all (again, except for Haskell).
+
+More similar exercise are found here: http://rosalind.info/problems/list-view/ (source)
+*/
+// My function
+// function DNAStrand(dna) {
+//   const output = [];
+//   const arr = dna.split("").map(x => {
+//     if (x === "A") {
+//       output.push("T");
+//     } else if (x === "T") {
+//       output.push("A");
+//     } else if (x === "C") {
+//       output.push("G");
+//     } else if (x === "G") {
+//       output.push("C");
+//     } else {
+//       output.push(x);
+//     }
+//   });
+//   return output.join("");
+// }
+
+// perfect way
+// function DNAStrand(dna) {
+//   return dna.replace(/./g, function(c) {
+//     return DNAStrand.pairs[c];
+//   });
+// }
+
+// DNAStrand.pairs = {
+//   A: 'T',
+//   T: 'A',
+//   C: 'G',
+//   G: 'C',
+// };
+
+// console.log(DNAStrand("AAAA"));// "TTTT","String AAAA is"
+// console.log(DNAStrand("ATTGC"));// "TAACG","String ATTGC is"
+// console.log(DNAStrand("GTAT"));// "CATA","String GTAT is"
