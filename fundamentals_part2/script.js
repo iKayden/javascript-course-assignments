3348db60ab5f30c1215ebc52d182f382d856c26c
ikayden <ikyden23@gmail.com>
2022-12-17 03:34:10 -0800

    Is this a triangle? challenge

--- fundamentals_part2/script.js
+++ fundamentals_part2/script.js
@@ -553,3 +553,15 @@ More similar exercise are found here: http://rosalind.info/problems/list-view/ (
 // console.log(DNAStrand("AAAA"));// "TTTT","String AAAA is"
 // console.log(DNAStrand("ATTGC"));// "TAACG","String ATTGC is"
 // console.log(DNAStrand("GTAT"));// "CATA","String GTAT is"
+
+
+/* Is this a triangle?
+Implement a function that accepts 3 integer values a, b, c. The function should return true if a triangle can be built with the sides of given length and false in any other case.
+
+(In this case, all triangles must have surface greater than 0 to be accepted). */
+
+// function isTriangle(a, b, c) {
+//   return a + b > c && a + c > b && b + c > a;
+// }
+// console.log(isTriangle(1, 2, 2)); //true;
+// console.log(isTriangle(7, 2, 2)); //false
