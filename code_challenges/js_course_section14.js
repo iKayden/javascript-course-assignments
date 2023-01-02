eedf5de965458e605979a2f6fd86fd1a05300926
ikayden <ikyden23@gmail.com>
2023-01-02 15:06:47 -0800

    work on Class Declaration Info

--- code_challenges/js_course_section14.js
+++ code_challenges/js_course_section14.js
@@ -7,7 +7,7 @@ const Person = function(firstName, birthYear) { //Can't use arrow func
   this.birthYear = birthYear;
 };
 // 1 New obj is created
-const kayden = new Person("Kayden", 1992);
+// const kayden = new Person("Kayden", 1992);
 // 2 function is called, this = {}
 // 3 {} linked to prototype
 // console.log(kayden);
@@ -39,8 +39,66 @@ Car.prototype.brake = function() {
   console.log(`${this.make} speed is down to ${this.speed} km/h`);
 };
 
-const bmw = new Car("BMW", 120);
-const mercedes = new Car("Mercedes", 95);
-bmw.accelerate();
-mercedes.accelerate();
-mercedes.brake();
+// const bmw = new Car("BMW", 120);
+// const mercedes = new Car("Mercedes", 95);
+// bmw.accelerate();
+// mercedes.accelerate();
+// mercedes.brake();
+
+// Class Declaration Info
+//  1. Classes are NOT hoisted (use after declaration)
+//  2. Classes are 1st class citizens (can pass and return from functions)
+//  3. Classes are executed in the strict mode
+class PersonCL {
+  constructor(fullName, birthYear) {
+    this.fullName = fullName;
+    this.birthYear = birthYear;
+  }
+  // Method (prototype) declaration in the class
+  calcAge() {
+    console.log(2037 - this.birthYear);
+  }
+
+  get age() {
+    return 2037 - this.birthYear;
+  }
+  // Pattern to set a property that already exists
+  set fullName(name) {
+    if (name.includes(" ")) this._fullName = name; // _ to avoid naming conflict
+    else console.log('Enter your full name');
+  }
+
+  get fullName() {
+    return this._fullName;
+  }
+}
+// Method (prototype) declaration outside of the class
+// PersonCL.prototype.greet = function() {
+//   console.log(`Hey, ${this.firstName}!`);
+// };
+
+const kayden = new PersonCL("Kayden Kharchenko", 1992);
+console.log(kayden);
+// kayden.calcAge();
+// kayden.greet();
+
+// console.log(kayden.age); // using getter from the class
+
+// -------------Getters And Setters----------------
+const account = {
+  owner: "Kayden",
+  movements: [200, 1000, 530, 142, 300],
+
+  get latest() {
+    return this.movements.slice(-1).pop();
+  },
+
+  set latest(mov) {
+    this.movements.push(mov);
+  }
+};
+// this is how setter works
+account.latest = 50;
+account.latest = 55;
+// this is a getter (last value in the arr)
+console.log(account.latest);
