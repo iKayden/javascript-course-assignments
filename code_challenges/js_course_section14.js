7b28b006547bef6bf7d7f6efef07f1c0bf551471
ikayden <ikyden23@gmail.com>
2023-01-02 17:08:54 -0800

    Finish code challenge 2

--- code_challenges/js_course_section14.js
+++ code_challenges/js_course_section14.js
@@ -45,7 +45,7 @@ Car.prototype.brake = function() {
 // mercedes.accelerate();
 // mercedes.brake();
 
-// Class Declaration Info
+//
 //  1. Classes are NOT hoisted (use after declaration)
 //  2. Classes are 1st class citizens (can pass and return from functions)
 //  3. Classes are executed in the strict mode
@@ -54,7 +54,7 @@ class PersonCL {
     this.fullName = fullName;
     this.birthYear = birthYear;
   }
-  // Method (prototype) declaration in the class
+  // Instance Method (prototype) declaration in the class
   calcAge() {
     console.log(2037 - this.birthYear);
   }
@@ -71,17 +71,22 @@ class PersonCL {
   get fullName() {
     return this._fullName;
   }
+  // Adding static methods (not available on the instances of the class, only on the class itself)
+  static hey() {
+    console.log(`Hey there, ${this}!`);
+  }
 }
-// Method (prototype) declaration outside of the class
+// Instance Method (prototype) declaration outside of the class
 // PersonCL.prototype.greet = function() {
 //   console.log(`Hey, ${this.firstName}!`);
 // };
 
 const kayden = new PersonCL("Kayden Kharchenko", 1992);
-console.log(kayden);
+// console.log(kayden);
 // kayden.calcAge();
 // kayden.greet();
-
+// kayden.hey() // WON'T WORK because used on the instance of the class
+// PersonCL.hey(); // Will work
 // console.log(kayden.age); // using getter from the class
 
 // -------------Getters And Setters----------------
@@ -101,4 +106,77 @@ const account = {
 account.latest = 50;
 account.latest = 55;
 // this is a getter (last value in the arr)
-console.log(account.latest);
+// console.log(account.latest);
+
+// ------------ Object.create ----------
+const PersonProto = {
+  calcAge() { //constructor function
+    console.log(2023 - this.birthYear);
+  },
+
+  init(name, birthYear) {
+    this.name = name,
+      this.birthYear = birthYear,
+      console.log(this.name, this.birthYear);
+  }
+};
+
+// const person = Object.create(PersonProto); //links the empty obj to the Class proto inheritance
+// person.name = "Bot";
+// person.birthYear = 1895;
+// console.log(person);
+// person.calcAge();
+
+// console.log(person.__proto__);
+
+// const kris = Object.create(PersonProto);
+// kris.init("Kris", 1995);
+// kris.calcAge();
+
+// Coding Challenge #2
+
+// 1. Redo Challenge 1 using ES6 class
+class CarCl {
+  constructor(make, speed) {
+    this.make = make;
+    this.speed = speed;
+    console.log(`This is a ${this.make}, it moves at ${this.speed} km/h`);
+  }
+  accelerate = function() {
+    this.speed += 10;
+    console.log(`${this.make} speed is up to ${this.speed} km/h`);
+  };
+  brake = function() {
+    this.speed -= 5;
+    console.log(`${this.make} speed is down to ${this.speed} km/h`);
+  };
+  // 2. Add a getter called "speedUS"
+  get speedUS() {
+    const miles = this.speed / 1.6;
+    console.log(`${this.speed} km/h is ${miles} mi/h`);
+    return miles;
+  }
+
+  currSpeed = function() {
+    console.log(`You are going at ${this.speed} km/h`);
+    return;
+  };
+
+  set currSpeed(s) { }
+
+  // 3. Add a setter called "speedUS"
+  set speedUS(mi) {
+    return this.speed = mi * 1.6;
+  }
+}
+
+
+// 4. Create a new car and experiment with methods
+const myCar = new CarCl("Ford", 120);
+myCar.accelerate();
+myCar.brake();
+myCar.speedUS;
+myCar.currSpeed();
+myCar.speedUS = 100;
+myCar.speedUS;
+myCar.currSpeed();
