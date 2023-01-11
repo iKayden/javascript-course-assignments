6ee00ded9e19e21d92c0b71d69f462da134b9dc6
ikayden <ikyden23@gmail.com>
2023-01-10 16:08:35 -0800

    finish part 1 code challenge 1

--- section16Async/script.js
+++ section16Async/script.js
@@ -24,75 +24,6 @@ const renderError = (msg) => {
   countriesContainer.insertAdjacentText("beforeend", msg);
 };
 
-// const getCountryData = (country) => {
-
-//   const url = `https://restcountries.com/v2/name/${country}`;
-//   // Old School Way
-//   const req = new XMLHttpRequest();
-//   req.open("GET", url);
-//   req.send();
-
-//   req.addEventListener("load", function(e) { // as soon as data arrives will be called
-//     const [data] = JSON.parse(this.responseText);
-//     console.log(data);
-//     const html = `
-//     <article class="country">
-//     <img class="country__img" src="${data.flag}" />
-//     <div class="country__data">
-//     <h3 class="country__name">${data.name}</h3>
-//     <h4 class="country__region">${data.region}</h4>
-//     <p class="country__row"><span>👫</span>${data.population.toLocaleString("en-US")}</p>
-//     <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
-//     <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
-//     </div>
-//     </article>
-//     `;
-//     countriesContainer.insertAdjacentHTML("beforeend", html);
-//     countriesContainer.style.opacity = 1;
-//   });
-// };
-
-// getCountryData("ukraine");
-
-
-// const getCountryAndNeighbourData = (country) => {
-//   // First AJAX call
-//   const url = `https://restcountries.com/v2/name/${country}`;
-//   // Old School Way
-//   const req1 = new XMLHttpRequest();
-//   req1.open("GET", url);
-//   req1.send();
-
-//   req1.addEventListener("load", function() { // as soon as data arrives will be called
-//     const [data] = JSON.parse(this.responseText);
-//     console.log(data);
-//     renderCountry(data);
-
-//     // Get Neighbor Countries
-//     const [neighbour] = data.borders;
-//     console.log(neighbour);
-//     // Second AJAX call
-//     const req2 = new XMLHttpRequest();
-//     const url = `https://restcountries.com/v2/alpha/${neighbour}`;
-//     req2.open("GET", url);
-//     req2.send();
-//     req2.addEventListener("load", function(e) {
-
-//       const data2 = JSON.parse(this.responseText);
-//       renderCountry(data2, "neighbour");
-//     });
-//   });
-// };
-
-// // getCountryAndNeighbourData("ukraine");
-// getCountryAndNeighbourData("canada");
-
-
-// // Old School Way
-// const req = new XMLHttpRequest();
-// req.open("GET", url);
-// req.send();
-
 const getJSON = function(url, errMsg = "Something went wrong 😭😭😭") {
   return fetch(url).then(rawData => {
     if (!rawData.ok) throw new Error(`${errMsg} (${rawData.status})`);
@@ -121,3 +52,19 @@ const getCountryData = function(country) {
 btn.addEventListener("click", function() {
   getCountryData("ukraine");
 });
+
+
+// Code Challenge #1
+//part 1
+const whereAmI = function(lat, lng) {
+  const url = `https://nominatim.openstreetmap.org/reverse?lat=${lat}&lon=${lng}&format=json`;
+  return fetch(url)
+    .then(rawData => rawData.json())
+    .then(data => getCountryData(data.address.country))
+    .catch(err => renderError(`${err.message}`));
+};
+
+// whereAmI(52.508, 13.381);
+// whereAmI(19.037, 72.873);
+whereAmI(-33.933, 18.474);
+
